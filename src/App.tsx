90b82968b75856f4e317bb8e4d0f86faa381282a
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-17 17:19:12 +0900

    :recycle:認証削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react";
-import { Auth } from "aws-amplify";
+import React from "react";
 
 function App() {
-  const [user, setUser] = useState<{ username: string } | null>(null);
-
-  useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then((user: { username: string }) => setUser(user))
-      .catch(() => setUser(null));
-  }, []);
-
   return (
     <main>
       <h1>Welcome to My App</h1>
-      {user ? (
-        <p>Hello, {user.username}!</p>
-      ) : (
-        <p>Please log in to see your content.</p>
-      )}
+      <p>Please log in to see your content.</p>
     </main>
   );
 }
