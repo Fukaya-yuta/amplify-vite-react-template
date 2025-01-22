2762fc78f8d442c09d5ceb2bde0a9e4cf9a74e65
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-22 16:38:21 +0900

    lambda関数型修正

--- amplify/functions/snowflake/resource.ts
+++ amplify/functions/snowflake/resource.ts
@@ -3,13 +3,13 @@ import * as path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Code, Function, Runtime, FunctionProps } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs"; // 追加
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
 export const sayHelloFunctionHandler = defineFunction(
-  (scope: Construct) => // 型を追加
+  (scope: Construct): FunctionProps => // 型を追加
     new Function(scope, "snowflake", {
       handler: "index.handler",
       runtime: Runtime.PYTHON_3_9,
