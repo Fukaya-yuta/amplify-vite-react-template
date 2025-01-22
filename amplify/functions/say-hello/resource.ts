de108d371b0db11feb5d5dcd56c82a734ba42ee0
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-22 16:43:22 +0900

    lambda関数型修正

--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -3,17 +3,16 @@ import * as path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
-import { Code, Function, Runtime, FunctionProps } from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs"; // 追加
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
 export const sayHelloFunctionHandler = defineFunction(
-  (scope: Construct): FunctionProps => // 型を追加
-    new Function(scope, "snowflake", {
+  (scope) =>
+    new Function(scope, "say-hello", {
       handler: "index.handler",
-      runtime: Runtime.PYTHON_3_9,
-      timeout: Duration.seconds(60),
+      runtime: Runtime.PYTHON_3_9, // or any other python version
+      timeout: Duration.seconds(20), //  default is 3 seconds
       code: Code.fromAsset(functionDir, {
         bundling: {
           image: DockerImage.fromRegistry("dummy"), // replace with desired image from AWS ECR Public Gallery
