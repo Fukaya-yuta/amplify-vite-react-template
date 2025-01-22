8c9af054f35ce41ddce0585eb7fcddba1864a80c
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-22 16:32:48 +0900

    lambda関数型修正

--- amplify/functions/snowflake/resource.ts
+++ amplify/functions/snowflake/resource.ts
@@ -4,28 +4,29 @@ import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs"; // 追加
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
 export const sayHelloFunctionHandler = defineFunction(
-    (scope) =>
-        new Function(scope, "snowflake", {
-        handler: "index.handler",
-        runtime: Runtime.PYTHON_3_9,
-        timeout: Duration.seconds(60),
-        code: Code.fromAsset(functionDir, {
-            bundling: {
-            image: DockerImage.fromRegistry("dummy"), // replace with desired image from AWS ECR Public Gallery
-            local: {
-                tryBundle(outputDir: string) {
-                execSync(
-                    `python3 -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
-                );
-                execSync(`cp -r ${functionDir}/* ${path.join(outputDir)}`);
-                return true;
-                },
+  (scope: Construct) => // 型を追加
+    new Function(scope, "snowflake", {
+      handler: "index.handler",
+      runtime: Runtime.PYTHON_3_9,
+      timeout: Duration.seconds(60),
+      code: Code.fromAsset(functionDir, {
+        bundling: {
+          image: DockerImage.fromRegistry("dummy"), // replace with desired image from AWS ECR Public Gallery
+          local: {
+            tryBundle(outputDir: string) {
+              execSync(
+                `python3 -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
+              );
+              execSync(`cp -r ${functionDir}/* ${path.join(outputDir)}`);
+              return true;
             },
-            },
-        }),
-        })
+          },
+        },
+      }),
+    })
 );
