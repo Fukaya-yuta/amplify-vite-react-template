0fded02743331c1767a386cfcf23b18e966cabdb
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-23 17:08:00 +0900

    lambdaソース更新対応

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,27 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
 import { HelloWorldLambdaStack } from './functions/helloworld/resources';
 
 const backend = defineBackend({
-  auth,
+  parameters: {
+    ProjectName: 'c-elect-meg-cloud2',
+    Environment: 'poc',
+    LambdaProtectedSubnet1: 'subnet-051a99ad5edb338a1',
+    LambdaProtectedSubnet2: 'subnet-08809bd4cc0d61ae6',
+    LambdaSecurityGroupID: 'sg-043e1d90db0533260',
+    LambdaArchiveBucketName: 'wireless-sensing-poc-lambda-archive-ap-northeast-1',
+    LambdaArchiveBucketObjectKey: 'lambda_for_snowflake_connect/lambda_function.zip',
+    LambdaArchiveObjectVersionID: '385Q2wcOZweFiJzbhH0WtL.iwD8GUqNb',
+    SnowflakeConnectLayerArn: 'arn:aws:lambda:ap-northeast-1:864981750279:layer:snowflake_connector_layer:4',
+    LambdaHandler: 'lambda_function.lambda_handler',
+    LambdaMemorySize: 256,
+    LambdaTimeout: 30,
+    LambdaRuntime: 'python3.9',
+    SSMParameterNameForSnowflakePassword: '/snowflake/password',
+    SSMParameterNameForSnowflakeAccount: '/snowflake/account',
+    SSMParameterNameForSnowflakeUser: '/snowflake/user',
+    SSMParameterNameForSnowflakeDatabase: '/snowflake/database',
+    SSMParameterNameForSnowflakeSchema: '/snowflake/schema',
+  },
 });
 
 // バックエンドに HelloWorld カスタム Lambda スタックを追加します。
--- amplify/functions/helloworld/resources.ts
+++ amplify/functions/helloworld/resources.ts
@@ -1,30 +1,98 @@
-import { CfnOutput, Stack, StackProps, Duration } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+interface HelloWorldLambdaStackProps extends StackProps {
+  projectName: string;
+  environment: string;
+  lambdaProtectedSubnet1: ec2.ISubnet;
+  lambdaProtectedSubnet2: ec2.ISubnet;
+  lambdaSecurityGroupID: ec2.ISecurityGroup;
+  lambdaArchiveBucketName: string;
+  lambdaArchiveBucketObjectKey: string;
+  lambdaArchiveObjectVersionID: string;
+  snowflakeConnectLayerArn: string;
+  lambdaHandler: string;
+  lambdaMemorySize: number;
+  lambdaTimeout: number;
+  lambdaRuntime: lambda.Runtime;
+  ssmParameterNameForSnowflakePassword: string;
+  ssmParameterNameForSnowflakeAccount: string;
+  ssmParameterNameForSnowflakeUser: string;
+  ssmParameterNameForSnowflakeDatabase: string;
+  ssmParameterNameForSnowflakeSchema: string;
+}
 
 export class HelloWorldLambdaStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: HelloWorldLambdaStackProps) {
+    super(scope, id, props);
 
-        // Lambda 関数を定義します
-        const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
-            runtime: lambda.Runtime.PYTHON_3_9, // ランタイムを指定します
-            handler: 'index.handler',            // ハンドラー関数を指定します
-            code: lambda.Code.fromAsset('./amplify/functions/helloworld'),
-            functionName: 'HelloWorldFunction',
-            description: 'This is my custom Lambda function created using CDK',
-            timeout: Duration.seconds(30),
-            memorySize: 128,
-            environment: {
-                TEST: 'test',
-            },
-        });
+    const lambdaRole = new iam.Role(this, 'SnowflakeConnectLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        LambdaAccessForSnowflakeConnect: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['ssm:GetParameter', 'ssm:GetParameters', 'kms:Decrypt'],
+              resources: ['*'],
+            }),
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['logs:CreateLogGroup'],
+              resources: [`arn:aws:logs:${this.region}:${this.account}:*`],
+            }),
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
+              resources: [`arn:aws:logs:${this.region}:${this.account}:log-group:/aws/lambda/${props.projectName}-${props.environment}-snowflake-connect:*`],
+            }),
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['ec2:CreateNetworkInterface', 'ec2:DescribeNetworkInterfaces', 'ec2:DeleteNetworkInterface'],
+              resources: ['*'],
+            }),
+          ],
+        }),
+      },
+    });
 
-        // Lambda 関数の ARN を出力します
-        new CfnOutput(this, 'HellowWorldFunctionArn', {
-            value: helloWorldFunction.functionArn,
-            exportName: 'HelloWorldFunctionArn',
-        });
-    }
+    const snowflakeConnectLambda = new lambda.Function(this, 'SnowflakeConnectLambda', {
+      vpc: ec2.Vpc.fromVpcAttributes(this, 'Vpc', {
+        vpcId: 'vpc-12345678',
+        availabilityZones: ['ap-northeast-1a', 'ap-northeast-1c'],
+        publicSubnetIds: ['subnet-051a99ad5edb338a1', 'subnet-08809bd4cc0d61ae6'],
+      }),
+      vpcSubnets: {
+        subnets: [props.lambdaProtectedSubnet1, props.lambdaProtectedSubnet2],
+      },
+      securityGroups: [props.lambdaSecurityGroupID],
+      code: lambda.Code.fromBucket(
+        s3.Bucket.fromBucketName(this, 'LambdaArchiveBucket', props.lambdaArchiveBucketName),
+        props.lambdaArchiveBucketObjectKey,
+        props.lambdaArchiveObjectVersionID
+      ),
+      handler: props.lambdaHandler,
+      runtime: lambda.Runtime.PYTHON_3_9,
+      memorySize: props.lambdaMemorySize,
+      timeout: Duration.seconds(props.lambdaTimeout),
+      environment: {
+        SNOWFLAKE_USER: props.ssmParameterNameForSnowflakeUser,
+        SNOWFLAKE_PASSWORD: props.ssmParameterNameForSnowflakePassword,
+        SNOWFLAKE_ACCOUNT: props.ssmParameterNameForSnowflakeAccount,
+        SNOWFLAKE_DATABASE: props.ssmParameterNameForSnowflakeDatabase,
+        SNOWFLAKE_SCHEMA: props.ssmParameterNameForSnowflakeSchema,
+      },
+      layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'SnowflakeConnectLayer', props.snowflakeConnectLayerArn)],
+      role: lambdaRole,
+    });
+
+    new CfnOutput(this, 'SnowflakeConnectLambdaArn', {
+      value: snowflakeConnectLambda.functionArn,
+      exportName: 'SnowflakeConnectLambdaArn',
+    });
+  }
 }
