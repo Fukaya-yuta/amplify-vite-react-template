f18db817717914b1fda0fb145c0f3c621f685120
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-23 11:27:39 +0900

    削除

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { hogeFunctionHandler } from './functions/say-hello/resource'; // 関数名を修正
 
 defineBackend({
   auth,
-  hogeFunctionHandler, // 関数名を修正
 });
