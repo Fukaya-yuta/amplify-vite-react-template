34c3c5c3cc11067fb41c80062d770fd86b946cf6
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-23 10:22:40 +0900

    tslambda追加検証

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
+import { sayHello } from './functions/say-hello/resource';
 
 defineBackend({
   auth,
+  sayHello
 });
