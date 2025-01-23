ee1c8f7b4a6283ff9e6e50a5912106c941367f83
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-23 17:25:47 +0900

    auth定義漏れの修正

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
 import { HelloWorldLambdaStack } from './functions/helloworld/resources';
 
 const backend = defineBackend({
