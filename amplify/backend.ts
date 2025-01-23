d3ae3cfdd5973ebd31505b32b0630cb20e10330a
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-01-23 16:03:12 +0900

    lambda(cdk追加)

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
+import { HelloWorldLambdaStack } from  './custom-functions/resources' ; 
 
 defineBackend({
   auth,
 });
+
+// バックエンドに HelloWorld カスタム Lambda スタックを追加します。
+new  HelloWorldLambdaStack ( 
+  backend.createStack ( 'HelloWorldLambdaStack' ), 'helloWorldLambdaResource' ,   {} );
