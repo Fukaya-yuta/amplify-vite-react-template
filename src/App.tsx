f4ca9d3b516f7dfe90fdf273926910970904abb1
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-02-04 16:28:38 +0900

    amplify_output出力先定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { useState, useEffect } from "react";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-const apiEndpoint = outputs.custom.apiGatewayInvokeURL;
-console.log("API Endpoint:", apiEndpoint);
-
 const App = () => {
+  const [apiEndpoint, setApiEndpoint] = useState("");
+
+  useEffect(() => {
+    const endpoint = outputs.custom.apiGatewayInvokeURL;
+    setApiEndpoint(endpoint);
+    console.log("API Endpoint:", endpoint);
+  }, []);
+
   return (
     <div>
       <h1>Hello, World!</h1>
+      <p>API Endpoint: {apiEndpoint}</p>
     </div>
   );
 };
