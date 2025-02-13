95bad04d89e4582ba4b73fdc58355ecce29470b7
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-02-13 16:12:11 +0900

    auth修正

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,6 +1,4 @@
 import { defineAuth } from '@aws-amplify/backend';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as cdk from 'aws-cdk-lib';
 
 /**
  * Define and configure your auth resource
@@ -11,22 +9,3 @@ export const auth = defineAuth({
     email: true,
   },
 });
-
-const stack = new cdk.Stack();
-export const userPool = new cognito.UserPool(stack, 'UserPool', {
-  selfSignUpEnabled: true,
-  signInAliases: { email: true },
-  autoVerify: { email: true },
-  standardAttributes: {
-    email: {
-      required: true,
-      mutable: false,
-    },
-  },
-});
-export const userPoolClient = new cognito.UserPoolClient(stack, 'UserPoolClient', {
-  userPool,
-});
-
-export const userPoolId = userPool.userPoolId;
-export const userPoolClientId = userPoolClient.userPoolClientId;
