e49d39fca55c0de9d83dfcdf03c56eaead4aa646
fukaya <fukaya.yuta@dx.mitsubishielectric.co.jp>
2025-02-17 16:23:35 +0900

    フロントエンドのCognito認証設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
-import { AuthSession, fetchAuthSession } from 'aws-amplify/auth';
+import { fetchAuthSession } from 'aws-amplify/auth';
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
